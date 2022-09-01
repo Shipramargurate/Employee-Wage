1581972bae896e6dbf0d867b74b94cccd56057cd
shipramargurate <shivanikumari22322@gmail.com>
2022-09-02 01:25:17 +0530

    [SHIVANI] : Added Employee Daily Wage

--- src/com/brideglab/empWage/EmployeeWage.java
+++ src/com/brideglab/empWage/EmployeeWage.java
@@ -4,14 +4,18 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Emplyoee Wage Computation Program");
 		
-		/* UC=1
-		 * checking employee is present or absent
+		/* 
+		 * UC2
+		 * Calculate Daily Employee Wage 
 		 */
 		int isPresent = 1;
+		int wagePerHour = 20;
+		int noOfWorkingHours = 8;
 		int checkAttendance =(int)Math.floor((Math.random()*10)%2);	
 		System.out.println(checkAttendance);
 		if(checkAttendance == isPresent) {
 			System.out.println("Emplyoee is present");
+			System.out.println("Emplyoee Daily wage is = " +wagePerHour*noOfWorkingHours);
 		}
 		else {
 			System.out.println("Emplyoee is absent");
